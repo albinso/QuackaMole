e7c5553ab6cdfb4d52266b8a1ea3475fd820f004
pnyberg <pernyb@kth.se>
2014-04-29 22:17:58 +0200

    Update Obstacle.java

--- Gamefield/Obstacle.java
+++ Gamefield/Obstacle.java
@@ -10,27 +10,45 @@ public class Obstacle {
 		this.height = height;
 	}
 
+	/**
+	 * The left/west side of the obstacle
+	 */
 	int getLeftSide() {
 		return x;
 	}
 
+	/**
+	 * The right/east side of the obstacle
+	 */
 	int getRightSide() {
 		return x + width;
 	}
 
+	/**
+	 * The up/north side of the obstacle
+	 */
 	int getUpSide() {
 		return y;
 	}
 
+	/**
+	 * The down/south side of the obstacle
+	 */
 	int getDownSide() {
 		return y + height;
 	}
 
+	/**
+	 * If given point is in the obstacle
+	 */
 	boolean inBounds(int x, int y) {
 		return this.x <= x && x < (this.x + width) && 
 				this.y <= y && y < (this.y + height);
 	}
 
+	/**
+	 * Draws the obstacle
+	 */
 	void draw(Graphics g) {
 		super.paintComponent(g);
 	}
