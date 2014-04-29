7465137610888b9d09257a3755ece113b160f56f
pnyberg <pernyb@kth.se>
2014-04-29 22:02:46 +0200

    Update Gamefield.java

--- Gamefield/Gamefield.java
+++ Gamefield/Gamefield.java
@@ -1,8 +1,14 @@
 import java.awt.*;
 
 interface Gamefield {
+	/**
+	 * Initiates the players and the game
+	 */
 	void intiGame();
 
+	/**
+	 * Initiates the field
+	 */
 	void initField();
 
 	void startGame();
