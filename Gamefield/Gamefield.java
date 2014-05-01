7d7e641a91ae73b0294deea813408003b42d26fc
pnyberg <per_skuru@msn.com>
2014-05-01 02:59:05 +0200

    lol

--- Gamefield/Gamefield.java
+++ Gamefield/Gamefield.java
@@ -1,12 +1,15 @@
 import java.awt.*;
+import java.awt.event.*;
 import java.io.*;
 
 interface Gamefield {
 	void initGame();
 
-	void initField(File mapFile);
+	void initField();
 
 	void startGame();
+
+	void actionPerformed(ActionEvent e);
 
 	void paintComponent(Graphics g);
 }
