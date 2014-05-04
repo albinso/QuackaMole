35ffce1dd84b4801eeb8a1bf0a50b9a6a53437c5
MisterWeasel <heerkip@gmail.com>
2014-05-04 20:45:46 +0200

    New TestClient

---
+++ SocketPainter - (pre QuackaMole post ChatClient)/TestClient.java
@@ -0,0 +1,68 @@
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+import java.io.*; // DEBUGG
+
+public class TestClient extends Socket {
+	private ObjectOutputStream outputStream;
+	private ObjectInputStream inputStream;
+	private String name;
+
+	public TestClient(SocketAddress adr, String name) throws IOException {
+		this.name = name;
+		try {
+			connect(adr);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		inputStream = new ObjectInputStream(getInputStream());
+		outputStream = new ObjectOutputStream(getOutputStream());
+	}
+
+// 	public void run() {
+// 		while(true) {
+// 			try {
+// 				String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
+// 				outputStream.writeObject(name + ": " + s);
+// //				Thread.sleep(1000);
+// 			} catch(IOException e) {
+// 				e.printStackTrace();
+// 				System.exit(-1);
+// //			} catch(InterruptedException e) {
+// //				e.printStackTrace();
+// 			}
+// 		}
+// 	}
+
+	public Object getObject() {
+		Object o = null;
+		try {
+			o = inputStream.readObject();
+		} catch(IOException e) {
+			e.printStackTrace();
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return o;
+	}
+
+	public void sendObject(Object o) {
+		try {
+			outputStream.writeObject(o);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+/*	public static void main(String[] args) throws IOException {
+		InetSocketAddress adr = new InetSocketAddress("192.168.0.100", 8087);
+
+		new TestClient(adr, args[0]);
+	}*/
+}
