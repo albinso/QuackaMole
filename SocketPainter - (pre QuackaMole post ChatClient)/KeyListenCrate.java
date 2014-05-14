29ddbc5c8b223c612267e47bc1dc266c1e8cad84
MisterWeasel <heerkip@gmail.com>
2014-05-14 16:53:17 +0200

    KeyListenCrate is commented.

--- SocketPainter - (pre QuackaMole post ChatClient)/KeyListenCrate.java
+++ SocketPainter - (pre QuackaMole post ChatClient)/KeyListenCrate.java
@@ -6,6 +6,10 @@ public class KeyListenCrate extends Obstacle {
 		super(x, y, getImages());
 	}
 
+	/**
+	* Just a place to read images and put them in an array.
+	* Only for use in constructor of superclass.
+	*/
 	private static ImageIcon[] getImages() {
 		ImageIcon[] images = new ImageIcon[1];
 		images[0] = new ImageIcon(Toolkit.getDefaultToolkit().getImage("crate_v2.png"));
