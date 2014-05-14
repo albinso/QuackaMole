209f24e912a51946be7e3ec00743f1465e85621f
pnyberg <per_skuru@msn.com>
2014-05-14 11:20:08 +0200

    lol

--- SocketPainter - (pre QuackaMole post ChatClient)/KeyListenCrate.java
+++ SocketPainter - (pre QuackaMole post ChatClient)/KeyListenCrate.java
@@ -1,5 +1,11 @@
+import java.awt.Image;
+
 public class KeyListenCrate extends BreakableObstacle {
 	public KeyListenCrate(int x, int y) {
-		super(x, y);
+		super(x, y, 0, getImages());
+	}
+
+	public Image[] getImages() {
+		return new Images[0];
 	}
 }
--- SocketPainter - (pre QuackaMole post ChatClient)/KeyListenDirt.java
+++ SocketPainter - (pre QuackaMole post ChatClient)/KeyListenDirt.java
@@ -1,5 +1,11 @@
+import java.awt.Image;
+
 public class KeyListenDirt extends BreakableObstacle{
 	public KeyListenDirt(int x, int y) {
-		super(x, y);
+		super(x, y, 0, getImages());
+	}
+
+	public Image[] getImages() {
+		return new Images[0];
 	}
 }
