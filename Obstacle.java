4c1da63923b54f3bcd108da1d39373059daad496
MisterWeasel <heerkip@gmail.com>
2014-05-14 10:26:46 +0200

    Obstacle color name change

---
+++ Obstacle.java
@@ -0,0 +1,40 @@
+package quack.block;
+import java.awt.*;
+
+public class Obstacle {
+	public static final int SIZE = 30; // default-value
+	protected Color color;
+	protected int x, y;
+
+	public Obstacle(int x, int y) {
+		this.x = x;
+		this.y = y;
+		color = Color.black;
+	}
+
+	public int getLeftSide() {
+		return x;
+	}
+
+	public int getRightSide() {
+		return x + SIZE;
+	}
+
+	public int getUpSide() {
+		return y;
+	}
+
+	public int getDownSide() {
+		return y + SIZE;
+	}
+
+	public boolean inBounds(int x, int y) {
+		return this.x <= x && x < getRightSide() && 
+				this.y <= y && y < getDownSide();
+	}
+
+	public void draw(Graphics g) {
+		g.setColor(color);
+		g.fillRect(x, y, SIZE, SIZE);
+	}
+}
