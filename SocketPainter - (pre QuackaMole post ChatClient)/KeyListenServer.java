b4d2720349fcd5346d3ae828af2e5fa14ff2e4ed
pnyberg <per_skuru@msn.com>
2014-05-15 21:30:48 +0200

    server and gui

--- SocketPainter - (pre QuackaMole post ChatClient)/KeyListenServer.java
+++ SocketPainter - (pre QuackaMole post ChatClient)/KeyListenServer.java
@@ -1,11 +1,39 @@
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class KeyListenServer extends JFrame {
+public class KeyListenServer extends JFrame implements ActionListener {
 	final int port = 8080; // the port used for the server
 
+	private JLabel usernameLabel, serverAddressLabel, portNumberLabel;
+	private JTextField usernameField, serverAddressField, portNumberField;
+	private JPanel buttonPanel;
+	private JButton startButton;
+
 	public KeyListenServer() {
-		startServer();
+		usernameLabel = new JLabel("Username");
+		serverAddressLabel = new JLabel("Server address");
+		portNumberLabel = new JLabel("Port number");
+		usernameField = new JTextField("Per");
+		serverAddressField = new JTextField("localhost");
+		portNumberField = new JTextField("8080");
+		buttonPanel = new JPanel();
+		startButton = new JButton("Start server!");
+
+		setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
+
+		add(usernameLabel);
+		add(usernameField);
+
+		buttonPanel.add(startButton);
+
+		add(buttonPanel);
 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(600, 600);
@@ -22,6 +50,11 @@ public class KeyListenServer extends JFrame {
 				}
 			}
 		}.start();
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == startButton)
+			startServer();
 	}
 
 	public static void main(String[] arg) {
---
+++ SocketPainter - (pre QuackaMole post ChatClient)/KeyListenServerBackend.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class KeyListenServerBackend extends ServerSocket implements Serializable {
+	private KeyListenLobby lobby;
+
+	public KeyListenServerBackend(int port) throws IOException {
+		super(port);
+
+		lobby = new KeyListenLobby();
+
+		lobby.start(); // starts the lobby
+		findClient();
+	}
+
+	/**
+	* Waits for a client to connect, add it to the lobby and then repeats the process.
+	*/
+	public void findClient() throws IOException {
+		while(true) {
+			Socket s = accept(); // find a socket (client)
+			System.out.println("Found a client, connecting!");
+
+			lobby.addClient(s);
+		}
+	}
+
+	public static void main(String[] arg) {
+	}
+}
